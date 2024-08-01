9e3f7e0a4d8287574d23e1eb9716aeca8517f428
Spindoctxr <thatomongale4@gmail.com>
2024-08-01 14:17:20 +0200

    ignore ts errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors: true,
+
+  },
   experimental: {
-    serverActions: true,
+    
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
